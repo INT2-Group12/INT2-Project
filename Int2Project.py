37f9db8ba385f23a5ab40809bbb339b31b688700
UnwiseCheese98 <72926190+UnwiseCheese98@users.noreply.github.com>
2021-04-28 13:25:59 +0100

    Dataset Loader
    imported libraries and loaded dataset

--- Int2Project.py
+++ Int2Project.py
@@ -1,1 +1,8 @@
+import torch
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision.datasets import CIFAR10
 
+dataset = CIFAR10(root='data/', download=True, train=True, transform=ToTensor())
+test_dataset = CIFAR10(root='data/', download=True, train=False, transform=ToTensor())
